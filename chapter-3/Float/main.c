0a8692de82985ee3b949756908b8f9cdbab4fb34
AbbasMehrabi97 <abbasmehrabi95@gmail.com>
2024-09-17 15:33:37 +0300

    finishing chapter 3

--- chapter-3/Float/main.c
+++ chapter-3/Float/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <math.h>
+#include <errno.h>
+#include <limits.h>
+//check if value is between signed char range
+errno_t do_stuff(signed long value) {
+ if ((value < SCHAR_MIN) || (value > SCHAR_MAX)) {
+      return ERANGE;
+ }
+ signed char sc = (signed char)value; // Cast quiets warning
+ //---snip---
+}
+
 const char *show_classification(double x) {
       switch(fpclassify(x)) {
       case FP_INFINITE: return "Inf";
@@ -14,4 +25,13 @@ int main(void){
       double x = (0 / 1);
       printf("%s\n",show_classification(x));
       return 0;
+
+      //Casting
+      int si = 5;
+      short ss = 8;
+      long sl = (long)si;
+      unsigned short us = (unsigned short)(ss + sl); //may causing error
+
+
+
 }
